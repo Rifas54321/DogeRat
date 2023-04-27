55113bae0d481434a04578d0fbdf7a1c1af5b4f1
Rifas54321 <93052567+Rifas54321@users.noreply.github.com>
2023-04-27 14:33:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '5879413766:AAEA8AMl_H565FuU91RhE1yAe17cFdGtjQc'
+const id = '2134964315'
 const address = 'https://www.google.com'
 
 const app = express();
